0ae3753e3795c3dade26485f52c2d49dfa1f669e
Braginiva <ivan.bragin7@gmail.com>
2026-04-19 19:35:31 +0500

    huyi

--- MetaHideApp/MetaHideApp/Model.cs
+++ MetaHideApp/MetaHideApp/Model.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MetaHide
+namespace MetaHide;
+
+internal class Model
 {
-    internal class Model
-    {
-    }
+    
 }
